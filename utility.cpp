0b07eee4d4e3c07f206d57ab29f009a3179e9510
Philippe Rerolle <prerolle@csil-11.cs.ucsb.edu>
2020-04-27 10:58:44 -0700

    Implement "isOdd" and "isEven"

---
+++ utility.cpp
@@ -0,0 +1,23 @@
+// utility.cpp
+
+// IN THIS FILE, define any of your OWN functions you may need to
+// solve the problems.
+
+// For example, if you need an isPrime function, you can put the function
+// definition in this file.  Similarly, isOdd or isEven might be useful.
+
+// You will need to include the function prototype in "utility.h" and
+// then be sure to  #include "utility.h" in the file where you use
+// these functions
+
+bool isOdd(int x) {
+  return x % 2;
+}
+
+bool isEven(int x) {
+  return !isOdd(x);
+}
+
+bool isPrime(int x) {
+  return false;  // REPLACE THIS STUB WITH REAL CODE
+}

1e8936bdf0b9bed6445f56db92a3886fc95dbe9e
Philippe Rerolle <prerolle@csil-11.cs.ucsb.edu>
2020-05-01 19:23:14 -0700

    Implement maxOfArray function and accompanying tests

--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -1,7 +1,19 @@
 #include "arrayFuncs.h"
 #include <cstdlib>
 #include <iostream>
+#include <cassert>
 
 int maxOfArray(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  if (size < 1) {
+    std::cerr << "ERROR: Called maxOfArray with size < 1" << std::endl;
+    exit(1);
+  }
+  int max = a[0];
+  for(int i = 1; i < size; i++) {
+    int current = a[i];
+    if (current > max) {
+      max = current;
+    }
+  }
+  return max; // STUB!  Replace with correct code.
 }
--- maxOfArrayErrorTest.cpp
+++ maxOfArrayErrorTest.cpp
@@ -3,8 +3,9 @@
 
 int main() {
 
-  // Fill this in with code that calls maxOfArray with a size of zero.
-  // so that you can see if the error message prints correctly.
+  int empty[] = {};
+  assertEquals(0, maxOfArray(empty, 0),
+               "maxOfArray(empty, 0)");
   return 0;
 
 }

116f75bac2378d20ff2a8e4ccc3a3c4ac1d92359
Philippe Rerolle <prerolle@csil-11.cs.ucsb.edu>
2020-05-01 17:42:20 -0700

    Implement countEvens

--- countEvens.cpp
+++ countEvens.cpp
@@ -1,7 +1,14 @@
+#include "utility.h"
 #include "arrayFuncs.h"
 #include <cstdlib>
 #include <iostream>
 
-int countEvens(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+int countEvens(int* a, int size) {
+  int evens = 0;
+  for (int i = 0; i < size; i++) {
+    if (isEven(a[i])) {
+      evens += 1;
+    }
+  }
+  return evens;
 }

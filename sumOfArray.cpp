b101c1aa3ec4d831da7eb092b7f5f1180cdaf9ed
Philippe Rerolle <prerolle@csil-11.cs.ucsb.edu>
2020-05-01 19:24:40 -0700

    Implement sumOfArrayMethod

--- sumOfArray.cpp
+++ sumOfArray.cpp
@@ -1,5 +1,9 @@
 #include "arrayFuncs.h"
 
 int sumOfArray(int a[], int size) {
-  return 0; // STUB!  Replace with correct code.
+  int sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += a[i];
+  }
+  return sum;
 }
